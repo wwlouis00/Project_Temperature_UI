e4b5282eba69353f52bc0c3fb8e330ad68c95e8d
wwlouis00 <wwlouis00@gmail.com>
2022-01-12 00:37:26 +0800

    update

---
+++ EGGI_Temperature/main.py
@@ -0,0 +1,13 @@
+import sys
+from Sulink_Temperature_ver1 import *   #執行
+
+# from Sulink_Temperature_20211217 import *   #執行
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    mainWindows = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWindows)
+    mainWindows.show()
+    sys.exit(app.exec_())
