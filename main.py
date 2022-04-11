110a8fb18a5bab1ab7fa64b96db756cfc5c57e3b
wwlouis00 <wwlouis00@gmail.com>
2022-04-11 11:54:05 +0800

    update 0411

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from Sulink_Temperature_ver1 import *   #執行
+# from Sulink_Temperature_ver1 import *   #執行
 
-# from Sulink_Temperature_20211217 import *   #執行
+from Sulink_Temperature_20220411 import *   #執行
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
