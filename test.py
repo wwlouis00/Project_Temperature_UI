3d4408e28ba433d8c2b13aafaa845a073439afd3
wwlouis00 <wwlouis00@gmail.com>
2022-05-25 18:05:40 +0800

    Update new result

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import os
-
-os.system("sshpass -p '123' scp ./data/factory.csv pi@192.168.50.238:/home/pi/socket_cam/result/")
+os.system("scp ./data/factory.csv pi@192.168.50.238:/home/pi/socket_cam/result/")
+#os.system("sshpass -p '123' scp ./data/factory.csv pi@192.168.50.238:/home/pi/socket_cam/result/")
